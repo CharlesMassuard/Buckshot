68751835ac115f904bef239c546e84efcc744550
ArnaudW31 <arnaudwq@gmail.com>
2026-06-02 10:45:30 +0200

    feat : envoi event data

--- lib/models/event_model.dart
+++ lib/models/event_model.dart
@@ -5,10 +5,13 @@ class EventModel {
   final String nom;
   final String description;
   final String lieu;
+  final String idOrganisateur;
   final int capaciteMax;
   final int placesRestantes;
   final DateTime dateHeureEvent;
+  final DateTime dateFinEvent;
   final DateTime dateOuvertureBilletterie;
+  final DateTime dateFermetureBilletterie;
 
   EventModel({
     required this.id,
@@ -16,9 +19,12 @@ class EventModel {
     required this.description,
     required this.lieu,
     required this.capaciteMax,
+    required this.idOrganisateur,
     required this.placesRestantes,
     required this.dateHeureEvent,
+    required this.dateFinEvent,
     required this.dateOuvertureBilletterie,
+    required this.dateFermetureBilletterie,
   });
 
   // Convertit un document Firestore en objet Dart exploitable
@@ -28,11 +34,29 @@ class EventModel {
       id: doc.id,
       nom: data['nom'] ?? '',
       description: data['description'] ?? '',
-      lieu: data['lieu'] ?? '',
+      lieu: data['lieu'] ?? "",
+      idOrganisateur: data['idOrganisateur'] ?? "",
       capaciteMax: data['capaciteMax'] ?? 0,
       placesRestantes: data['placesRestantes'] ?? 0,
       dateHeureEvent: (data['dateHeureEvent'] as Timestamp).toDate(),
+      dateFinEvent: (data['dateFinEvent'] as Timestamp).toDate(),
+      dateFermetureBilletterie: (data['dateFinEvent'] as Timestamp).toDate(), //ntm c pa ds le bon sens
       dateOuvertureBilletterie: (data['dateOuvertureBilletterie'] as Timestamp).toDate(),
     );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'nom': nom,
+      'description': description,
+      'lieu': lieu,
+      'idOrganisateur': idOrganisateur,
+      'capaciteMax': capaciteMax,
+      'placesRestantes': placesRestantes,
+      'dateHeureEvent': dateHeureEvent,
+      'dateFinEvent': dateFinEvent,
+      'dateFermetureBilletterie': dateFermetureBilletterie,
+      'dateOuvertureBilletterie': dateOuvertureBilletterie,
+    };
   }
 }
